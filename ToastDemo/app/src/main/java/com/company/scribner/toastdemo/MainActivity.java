9731a5bc8d87188bc80a22d8743f49940b2d0a3f
wescrib <scribner.william@gmail.com>
2018-06-10 22:32:08 -0400

    committing first thing that's kinda worth showing

---
+++ ToastDemo/app/src/main/java/com/company/scribner/toastdemo/MainActivity.java
@@ -0,0 +1,37 @@
+package com.company.scribner.toastdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
+    boolean flag = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.pusheen);
+    }
+
+    public void btnPressed(View v){
+        switch(v.getId()) {
+            case R.id.button: {
+                if (flag) {
+                    image.setImageResource(R.drawable.pusheen);
+                    flag = false;
+                    Toast.makeText(this,"FLUFFY!", Toast.LENGTH_SHORT).show();
+                } else {
+                    image.setImageResource(R.drawable.muscle_pusheen);
+                    flag = true;
+                    Toast.makeText(this,"MUSCLES!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+}
