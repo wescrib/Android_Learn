0c0c243e288ed0b0dfe9f5035382ce8ab4dcb4aa
wescrib <scribner.william@gmail.com>
2018-06-18 16:40:31 -0400

    committing audio

---
+++ Day_8/AudioProj/Audio/app/src/main/java/com/company/scribner/audio/MainActivity.java
@@ -0,0 +1,29 @@
+package com.company.scribner.audio;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+
+    public void playSound(View v){
+        mediaPlayer.start();
+
+    }
+
+    public void pause(View v){
+        mediaPlayer.pause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this,R.raw.zips8);
+
+    }
+}
