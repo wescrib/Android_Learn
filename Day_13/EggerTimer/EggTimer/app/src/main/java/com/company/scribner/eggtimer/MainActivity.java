190fbb57c7ce5e89c556e05ad7988650cc2cd7e5
wescrib <scribner.william@gmail.com>
2018-06-24 18:52:23 -0400

    built new countdown for egg timer

--- Day_13/EggerTimer/EggTimer/app/src/main/java/com/company/scribner/eggtimer/MainActivity.java
+++ Day_13/EggerTimer/EggTimer/app/src/main/java/com/company/scribner/eggtimer/MainActivity.java
@@ -1,13 +1,43 @@
 package com.company.scribner.eggtimer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void startCountDown(View view){
-        
+    TextView displayTimeLeft;
+
+    Button startBtn;
+
+    boolean isStarted = false;
+
+    public void startCountdown(View view){
+
+        displayTimeLeft = findViewById(R.id.timeLeft);
+        startBtn = findViewById(R.id.startButton);
+
+        CountDownTimer countDownTimer = new CountDownTimer(30000, 1000) {
+            public void onTick(long msUntilDone) {
+                displayTimeLeft.setText(String.valueOf(msUntilDone / 1000));
+            }
+
+            public void onFinish() {
+
+            }
+        };
+
+        if(!isStarted) {
+            startBtn.setText("Stop");
+            countDownTimer.start();
+
+            isStarted = true;
+        }
     }
 
     @Override
