bf997b501a9872d661087438cf3a1e9ce9316c62
wescrib <scribner.william@gmail.com>
2018-06-24 17:42:10 -0400

    handler/runnable timer

---
+++ Day_13/TimerDemo/app/src/main/java/com/company/scribner/timerdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.company.scribner.timerdemo;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Time", "A second passed by");
+
+                handler.postDelayed(this, 1000);
+            }
+        };
+
+        handler.post(run);
+    }
+}
