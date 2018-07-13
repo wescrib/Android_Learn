b189a9852c809d104e4d39dfd4ceb331a3164fde
wescrib <scribner.william@gmail.com>
2018-07-13 18:16:28 -0400

    finished dialog alert

--- Day_26/AlertDialog/app/src/main/java/com/company/scribner/alertdialog/MainActivity.java
+++ Day_26/AlertDialog/app/src/main/java/com/company/scribner/alertdialog/MainActivity.java
@@ -1,7 +1,10 @@
 package com.company.scribner.alertdialog;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("ALERT: ARE YOU SURE?")
+                .setMessage("Bro, think before you do")
+                .setPositiveButton("YES!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "Completed", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("nah...", null)
+                .show();
+
     }
 }
