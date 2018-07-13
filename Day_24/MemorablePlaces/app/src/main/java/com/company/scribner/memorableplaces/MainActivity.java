d5e5bdd4de47a600bdf2750c0f61c7a63aa43586
wescrib <scribner.william@gmail.com>
2018-07-13 17:47:41 -0400

    added shared preferences

--- Day_24/MemorablePlaces/app/src/main/java/com/company/scribner/memorableplaces/MainActivity.java
+++ Day_24/MemorablePlaces/app/src/main/java/com/company/scribner/memorableplaces/MainActivity.java
@@ -1,27 +1,27 @@
  package com.company.scribner.memorableplaces;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
+
 
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 
  public class MainActivity extends AppCompatActivity {
 
     ListView listView;
 
-    static ArrayList<String> locs = new ArrayList<String>();
-
-    static ArrayList<LatLng> coords = new ArrayList<LatLng>();
-
+     static ArrayList<String> places = new ArrayList<String>();
+     static ArrayList<LatLng> locations = new ArrayList<LatLng>();
      static ArrayAdapter arrayAdapter;
 
     @Override
@@ -29,11 +29,39 @@ import java.util.Arrays;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.listView);
-        locs.add("Add a new place!");
-        coords.add(new LatLng(0,0));
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.company.scribner.memorableplaces", Context.MODE_PRIVATE);
 
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, locs);
+        ArrayList<String> lats = new ArrayList<>();
+        ArrayList<String> longs = new ArrayList<>();
+
+        places.clear();
+        lats.clear();
+        longs.clear();
+        locations.clear();
+
+        try{
+            places = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("places", ObjectSerializer.serialize(new ArrayList<String>())));
+            lats = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("latitudes", ObjectSerializer.serialize(new ArrayList<String>())));
+            longs = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("longitudes", ObjectSerializer.serialize(new ArrayList<String>())));
+        }catch(Exception e) {
+
+            e.printStackTrace();
+        }
+        if(places.size() > 0 && lats.size() > 0 && longs.size() > 0){
+            if(places.size() == lats.size() && places.size() == longs.size()){
+                for(int i=0; i< lats.size(); i++){
+                    locations.add(new LatLng(Double.parseDouble(lats.get(i)), Double.parseDouble(longs.get(i))));
+                }
+            }
+        }else{
+            places.add("Add a new place!");
+            locations.add(new LatLng(0,0));
+        }
+
+        listView = findViewById(R.id.listView);
+
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, places);
 
         listView.setAdapter(arrayAdapter);
 
--- Day_24/MemorablePlaces/app/src/main/java/com/company/scribner/memorableplaces/MapsActivity.java
+++ Day_24/MemorablePlaces/app/src/main/java/com/company/scribner/memorableplaces/MapsActivity.java
@@ -3,8 +3,8 @@ package com.company.scribner.memorableplaces;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.graphics.Camera;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -25,6 +25,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -113,9 +114,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         }else{
             Location placeLocation = new Location (LocationManager.GPS_PROVIDER);
-            placeLocation.setLatitude(MainActivity.coords.get(intent.getIntExtra("placeNum",0)).latitude);
-            placeLocation.setLongitude(MainActivity.coords.get(intent.getIntExtra("placeNum", 0)).longitude);
-            centerMapOnLoc(placeLocation, MainActivity.locs.get(intent.getIntExtra("placeNum", 0)));
+            placeLocation.setLatitude(MainActivity.locations.get(intent.getIntExtra("placeNum",0)).latitude);
+            centerMapOnLoc(placeLocation, MainActivity.places.get(intent.getIntExtra("placeNum", 0)));
         }
     }
 
@@ -147,11 +147,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             address += sdf.format(new Date());
         }
 
-        MainActivity.locs.add(address);
-        MainActivity.coords.add(latLng);
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+
+        MainActivity.places.add(address);
+        MainActivity.locations.add(latLng);
         MainActivity.arrayAdapter.notifyDataSetChanged();
 
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.company.scribner.memorableplaces", Context.MODE_PRIVATE);
+
+        try{
+            ArrayList<String> latitudes = new ArrayList<>();
+            ArrayList<String> longitudes = new ArrayList<>();
+
+            for(LatLng coord : MainActivity.locations){
+                latitudes.add(Double.toString(coord.latitude));
+                longitudes.add(Double.toString(coord.longitude));
+            }
+
+            sharedPreferences.edit().putString("places",ObjectSerializer.serialize(MainActivity.places)).apply();
+            sharedPreferences.edit().putString("lats",ObjectSerializer.serialize(latitudes)).apply();
+            sharedPreferences.edit().putString("lons",ObjectSerializer.serialize(longitudes)).apply();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
         Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
-        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+
     }
 }
