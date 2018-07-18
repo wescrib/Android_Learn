aa3155131ced2e767d0624e90a52a608254d42d0
wescrib <scribner.william@gmail.com>
2018-07-17 20:11:22 -0400

    complete. twas easy

--- Day_29/WebView/app/src/main/java/com/company/scribner/webview/MainActivity.java
+++ Day_29/WebView/app/src/main/java/com/company/scribner/webview/MainActivity.java
@@ -2,6 +2,8 @@ package com.company.scribner.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+//        webView.loadUrl("https://www.google.com");
+
+        webView.loadData("<html><body><h1>Hey Will, you should just give up.</h1></body></html","text/html", "UTF-8");
     }
 }
