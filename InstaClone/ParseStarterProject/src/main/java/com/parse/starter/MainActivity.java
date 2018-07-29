b579f2c641349aeba110853d9420d0986156ecb5
wescrib <scribner.william@gmail.com>
2018-07-29 16:14:37 -0400

    create and find examples

--- InstaClone/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ InstaClone/ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -14,11 +14,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Switch;
 
+import com.parse.GetCallback;
 import com.parse.LogInCallback;
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
@@ -30,7 +33,34 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+/*************SAVE STUFF ****************/
+//      ParseObject score = new ParseObject("Score");
+//      score.put("username", "nick");
+//      score.put("score", 45);
+//      score.saveInBackground(new SaveCallback() {
+//          @Override
+//          public void done(ParseException e) {
+//              if(e == null){
+//                  Log.i("Success", "saved test score");
+//              }else{
+//                  e.printStackTrace();
+//              }
+//          }
+//      });
 
+      /*******************GET STUFF *******************************/
+//      ParseQuery<ParseObject> query = ParseQuery.getQuery("Score");
+//
+//      query.getInBackground("3XkFs1Ltnb", new GetCallback<ParseObject>() {
+//          @Override
+//          public void done(ParseObject object, ParseException e) {
+//              if( e == null && object != null){
+//                  object.getString("username");
+//                  Log.i("USERNAME", object.getString("username"));
+//                  Log.i("SCORE", Integer.toString(object.getInt("score")));
+//              }
+//          }
+//      });
     
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
