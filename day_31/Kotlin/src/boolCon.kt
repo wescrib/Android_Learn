a76201e76c2dce23917a12b38fafc36ce73ed249
wescrib <scribner.william@gmail.com>
2018-11-23 19:09:57 -0600

    small condition and bool script

---
+++ day_31/Kotlin/src/boolCon.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+    var isAlive:Boolean = true
+    isAlive = !isAlive
+
+//    if(isAlive){
+//        println("yay, you're alive!")
+//    }else{
+//        println("bruh, you're dead")
+//    }
+
+    var name:String = "william"
+    if(name == "william"){
+        println("you are a total boss!!!!")
+    }else{
+        println("your life is a waste")
+    }
+}
