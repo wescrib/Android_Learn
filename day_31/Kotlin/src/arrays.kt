ed9a1a07a08144b19b55f1c275d4f2075c7e8665
wescrib <scribner.william@gmail.com>
2018-11-23 19:35:02 -0600

    lists

---
+++ day_31/Kotlin/src/arrays.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+    var nums = arrayListOf<Int>(4,6,32,4,6,7)
+    nums.add(3, 8)
+    var favoriteMovies = arrayListOf<String>("Speed","The Grand Budapest Hotel", "Logan","Memento")
+    println(favoriteMovies)
+    favoriteMovies.add(3,"Prisoners")
+    println(favoriteMovies)
+}
