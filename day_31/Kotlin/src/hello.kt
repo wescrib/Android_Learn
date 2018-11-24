00494f08a62bb26b58c6c45e784d3ffa4d29bc27
wescrib <scribner.william@gmail.com>
2018-11-23 18:47:33 -0600

    pushing day 31. first kotlin code

---
+++ day_31/Kotlin/src/hello.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+//        println("hello world")
+
+        var age = 26
+//        println(age)
+        age = 31
+//        println(age)
+        var place = "romania"
+        var firstName = "william"
+        var msg:String = "My name is ${firstName.capitalize()} and I want to go back to ${place.capitalize()}"
+
+        println(msg)
+}
