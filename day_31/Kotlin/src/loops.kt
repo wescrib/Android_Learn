454f9e887be0916a1e3a91d087ab83ddd3c30447
wescrib <scribner.william@gmail.com>
2018-11-27 16:35:59 -0700

    loops added

---
+++ day_31/Kotlin/src/loops.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+//    for(x in 1..10){
+//        println(x)
+//    }
+
+//    var list = listOf<String>("The Fast and the Furious", "2 Fast 2 Furious", "Tokyo Drift", "Fast and Furious", "Fast 5", "Fast and Furious 6")
+//
+//    for(ff in list){
+//        println(ff)
+//    }
+
+    for(n in 1..200){
+        if(n%2 != 0){
+            println(n)
+        }
+    }
+}
