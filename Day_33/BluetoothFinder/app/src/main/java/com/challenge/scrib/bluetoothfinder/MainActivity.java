d8e4447bb4cc3645f2df1886d1b3820162bf0541
wescrib <scribner.william@gmail.com>
2019-05-11 06:58:56 +0430

    still working on bluetooth connectivity

---
+++ Day_33/BluetoothFinder/app/src/main/java/com/challenge/scrib/bluetoothfinder/MainActivity.java
@@ -0,0 +1,65 @@
+package com.challenge.scrib.bluetoothfinder;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private Button searchBtn;
+    private TextView statusTextView;
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.i("Action", action);
+        }
+    };
+
+    BluetoothAdapter bluetoothAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        searchBtn = findViewById(R.id.searchBtn);
+        statusTextView = findViewById(R.id.statusTextView);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+
+        registerReceiver(broadcastReceiver, intentFilter);
+
+        bluetoothAdapter.startDiscovery();
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i("onClick", "searchBtn clicked: ");
+                statusTextView.setText("Searching...");
+                searchBtn.setEnabled(false);
+
+            }
+        });
+
+    }
+}
